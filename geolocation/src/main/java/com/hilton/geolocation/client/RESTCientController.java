e358102520f85e9261fccb558f079e625612f6cc
styadp92 <skothwal92@gmail.com>
2022-03-28 15:34:04 -0400

    Input param fix

--- geolocation/src/main/java/com/hilton/geolocation/client/RESTCientController.java
+++ geolocation/src/main/java/com/hilton/geolocation/client/RESTCientController.java
@@ -18,30 +18,31 @@ import com.hilton.geolocation.resources.LocationDetails;
 @Produces(MediaType.TEXT_PLAIN)
 @Path("/{query}")
 public class RESTCientController {
-	  private static final Logger LOGGER = LoggerFactory.getLogger(RESTCientController.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(RESTCientController.class);
 
 	private Client client;
-	
+
 	public RESTCientController(Client client) {
 		this.client = client;
 	}
-	
+
 	@GET
+
 	@Produces(MediaType.APPLICATION_JSON)
 	public LocationDetails getDetails(@PathParam("query") String ipAdr) {
 		System.out.println("IN GET DETAILS");
-		WebTarget wt = client.target("http://ip-api.com/json/");
+		WebTarget wt = client.target("http://ip-api.com/json/" + ipAdr);
 		Invocation.Builder ib = wt.request(MediaType.APPLICATION_JSON);
 		Response resp = ib.get();
 		LOGGER.info("Response Entity :: " + resp.getEntity().toString());
-		
+
 		LocationDetails ld = resp.readEntity(LocationDetails.class);
 
-		//String s = resp.readEntity(String.class);
-		
-		
+		// String s = resp.readEntity(String.class);
+
 		return ld;
-		
+
 	}
-	
+	 
+
 }
